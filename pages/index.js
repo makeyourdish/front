54bee624a74cb98837a108c7cc7ce7f1bae29212
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-11-23 17:01:01 +0100

    test nodeJs v18.12.1

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,12 @@ import Layout from "../src/components/Layout"
 const HomePage = () => {
   return (
     <Layout page="Accueil">
-      <p>Ceci est la page d'accueil !!!!</p>
+      <div className="w-screen h-screen flex items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold">Make Your Dish</h1>
+          <p className="text-2xl">🍷 Page d'accueil à venir 🍖</p>
+        </div>
+      </div>
     </Layout>
   )
 }
