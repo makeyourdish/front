5d52db342ae237fad185b28adfd7e1b66218d13f
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-12-02 20:17:22 +0100

    change admin header icon

--- src/components/AdminHeader.jsx
+++ src/components/AdminHeader.jsx
@@ -2,17 +2,20 @@ import Link from "next/link"
 import Image from "next/image"
 // import { useContext } from "react"
 import { IoCaretBackOutline } from "react-icons/io5"
+import { MdWeb } from "react-icons/md"
 // import AppContext from "./AppContext"
 
 const AdminHeader = () => {
   // const { session } = useContext(AppContext)
+  // let accountUserName = null
+  //todo: remove this line when signin exist
 
-  // let accountEmail = null
   let userName = "SokouPM"
 
   // if (session) {
   //   userName = JSON.parse(session).userName
   // }
+  //todo: remove this line when signin exist
 
   return (
     <header className="bg-gray-800 flex items-center justify-between px-4 py-2">
@@ -32,8 +35,9 @@ const AdminHeader = () => {
 
       <div className="flex items-center justify-center text-white text-xl">
         <Link href="/">
-          <a className="flex items-center justify-center border p-2 rounded transition-all hover:bg-white hover:text-black focus:outline focus:outline-3 focus:outline-white/75">
-            <IoCaretBackOutline className="mr-2" />
+          <a className="flex items-center justify-center md:border md:p-2 rounded transition-all hover:bg-white hover:text-black focus:outline focus:outline-3 focus:outline-white/75">
+            <IoCaretBackOutline className="hidden md:block mr-2" />
+            <MdWeb className="md:hidden block text-4xl" />
             <p className="hidden md:block">Retourner sur le site</p>
           </a>
         </Link>
