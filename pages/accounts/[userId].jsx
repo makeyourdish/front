b621fe828527563b953d83040891fb8a7e4c7c58
DylanGil <dylan.flame.libra@gmail.com>
2022-12-08 21:26:54 +0100

    mes informations UI done (no spatule poele)

--- pages/accounts/[userId].jsx
+++ pages/accounts/[userId].jsx
@@ -4,6 +4,7 @@ import AppContext from "../../src/components/AppContext"
 import Layout from "../../src/components/Layout"
 import api from "../../src/components/services/api"
 import { FiAlertTriangle } from "react-icons/fi"
+import Link from "next/link"
 
 const AccountPage = () => {
   const [user, setUser] = useState(null) //the user to display
@@ -19,7 +20,6 @@ const AccountPage = () => {
     account = JSON.parse(session).payload
   }
 
-  console.log("userId", userId)
   useEffect(() => {
     if (account && !isNaN(userId)) {
       api
@@ -39,8 +39,8 @@ const AccountPage = () => {
       <Layout page={`Profil: ${userId}`} pagetheme="food" screensize={+true}>
         <section>
           <div className="w-full mb-7 py-2 flex items-center justify-center text-red-600 text-center font-bold text-2xl rounded">
-            <FiAlertTriangle className="text-5xl mr-3" /> Veuillez vous
-            connecter
+            <FiAlertTriangle className="text-5xl mr-3" />
+            Veuillez vous connecter
           </div>
         </section>
       </Layout>
@@ -58,29 +58,49 @@ const AccountPage = () => {
     )
   }
 
+  const buttonStyle =
+    "bg-white text-center px-9 py-2 md:px-12 md:py-3 md:text-xl secondary-font rounded-2xl drop-shadow-md transition transform hover:scale-110 hover:bg-white/75"
+
   return (
     <Layout page={`Profil: ${userId}`} pagetheme="food" screensize={+true}>
-      {/* <AccountPageContent user={user} account={account} /> <- */}
-      {user?.userId === account?.userId || account.userAdmin ? (
-        <div>
-          <ul>
-            <li>
-              Id: <span>{user?.userId}</span>
-            </li>
-            <li>
-              Email: <span>{user?.userEmail}</span>
-            </li>
-            <li>
-              UserName: <span>{user?.userName}</span>
-            </li>
-            <li>
-              IsAdmin: <span>{user?.userAdmin.toString()}</span>
-            </li>
-          </ul>
-        </div>
-      ) : (
-        <h2>Vous n'etes pas autorisé</h2>
-      )}
+      <div className="flex flex-col justify-between items-center h-screen">
+        {/* <AccountPageContent user={user} account={account} /> <- */}
+        {user?.userId === account?.userId || account.userAdmin ? (
+          <div>
+            <div className="my-10 text-3xl md:text-3xl text-center md:mt-24">
+              <h2 className="secondary-font">Mes informations</h2>
+            </div>
+
+            <div className="tertiary-font text-center text-lg">
+              <p className="mb-4">
+                Nom d'utilisateur: <b>{user.userName}</b>
+              </p>
+              <p className="mb-4">
+                Mail: <b>{user.userEmail}</b>
+              </p>
+              {/*<p className="mb-4">Id: {user.userId}</p>
+            <p className="">IsAdmin: {user.userAdmin.toString()}</p> */}
+            </div>
+
+            <div className="flex flex-col md:flex-row z-30">
+              <Link href="/recipes/make">
+                <a className={`${buttonStyle} mb-4 md:mb-0 md:mr-4`}>
+                  Modifier mes informations
+                </a>
+              </Link>
+              <br></br>
+              <Link href="/recipes/make">
+                <a className={`${buttonStyle} mb-4 md:mb-0 md:mr-4`}>
+                  Modifier mon mot de passe
+                </a>
+              </Link>
+            </div>
+          </div>
+        ) : (
+          <h2>Vous n'etes pas autorisé</h2>
+        )}
+        <div className="w-screen flex flex-col md:flex-row items-center md:items-end pb-6 justify-end md:justify-center text-xl md:text-2xl text-center tertiary-font signin-flamme-background min-h-[250px] md:min-h-[280px]"></div>
+      </div>
     </Layout>
   )
 }
