02861fb3eb5793acb8b89e1bfd583540ee494d33
Pierre Marquet <marquet_pierre@yahoo.fr>
2022-12-22 11:15:20 +0100

    Update list.jsx

--- pages/list.jsx
+++ pages/list.jsx
@@ -2,9 +2,11 @@ import ListItems from "../src/components/ListItems"
 
 const List = () => {
   let queryString
+
   if (typeof window !== "undefined") {
     queryString = window.location.search
   }
+  
   return queryString === "?drink" ? (
     <ListItems pageTheme="drink" />
   ) : (
